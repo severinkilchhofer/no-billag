4e78c482bcd7795d64aaf908b7a54ba8be7df1d9
Severin Kilchhofer <severinkilchhofer@HFIAD17-severin-kilchhofer.local>
2018-01-13 10:48:13 +0100

    added jquery library added scrollify.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-$(document).foundation()
+$(document).foundation();
+
+$.scrollify({
+    section : "section",
+    sectionName : "section-name",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+});
