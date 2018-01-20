3f215b0049bec070359c61141d9bf1cd76ffc687
Severin Kilchhofer <severinkilchhofer@HFIAD17-severin-kilchhofer.local>
2018-01-20 16:11:32 +0100

    added header js added feed hover

--- js/app.js
+++ js/app.js
@@ -1,32 +1,11 @@
 $(document).foundation();
 
-$(function () {
-    $.scrollify({
-        section: ".view-section"
-    });
-});
-
-$.scrollify({
-    section : "section",
-    sectionName : "section-name",
-    interstitialSection : "",
-    easing: "easeOutExpo",
-    scrollSpeed: 1100,
-    offset : 0,
-    scrollbars: true,
-    standardScrollElements: "",
-    setHeights: true,
-    overflowScroll: true,
-    updateHash: true,
-    touchScroll:true,
-    before:function() {},
-    after:function() {},
-    afterResize:function() {},
-afterRender:function() {}
-});
-
 $(document).ready(function () {
-    $('input[type=button]').click(function () {
-        $('#toggleEvent').toggleClass('active');
-    });
+    var el = $('.innerfade li'),
+        i = 0;
+    $(el[0]).show();
+
+    (function loop() {
+        el.delay(200).fadeOut(0).eq(++i % el.length).fadeIn(0, loop);
+    }());
 });
