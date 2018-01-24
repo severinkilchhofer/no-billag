2776714736addb2c7a5e6ea23e2aa9cad28a79e3
Severin Kilchhofer <severinkilchhofer@HFIAD17-severin-kilchhofer.local>
2018-01-24 16:27:38 +0100

    added new feed data added links for fullscreen feed changed footer stylings added impressum

--- js/app.js
+++ js/app.js
@@ -6,6 +6,6 @@ $(document).ready(function () {
     $(el[0]).show();
 
     (function loop() {
-        el.delay(200).fadeOut(0).eq(++i % el.length).fadeIn(0, loop);
+        el.delay(150).fadeOut(0).eq(++i % el.length).fadeIn(0, loop);
     }());
 });
